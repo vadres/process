10410cc6f7ca28cbe7131a85ab62a91fe2fb050e
Victor Costa <vpassos10@hotmail.com>
2021-06-03 15:43:01 -0300

    fix: change migration V2

--- process-backend/src/main/resources/db/migration/V2__create_process_tables.sql
+++ process-backend/src/main/resources/db/migration/V2__create_process_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE tb_process (
 	id_process SERIAL PRIMARY KEY,
 	vl_description CHARACTER VARYING(100) NOT NULL,
 	
-	id_status INT8 NOT NULL, 
+	id_status INTEGER NOT NULL, 
 	dt_created_at timestamp WITHOUT time ZONE,
 	dt_updated_at timestamp WITHOUT time ZONE
 );
@@ -17,7 +17,7 @@ FOREIGN KEY (id_status) REFERENCES tb_process_status;
 
 CREATE TABLE tb_process_sight (
 	id_process_sight SERIAL PRIMARY KEY,
-	id_process INT8 NOT NULL
+	id_process INTEGER NOT NULL
 );
 
 ALTER TABLE tb_process_sight ADD CONSTRAINT process_sight_fk 
