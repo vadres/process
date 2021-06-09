3ed224b457ce444117205002a33a2d5db81d56e7
Victor Costa <antonio.vcp@isgh.org.br>
2021-06-09 13:47:54 -0300

    refact: remove folder bin/ created on windows SO

--- process-backend/bin/src/main/resources/db/migration/V1.3__update_password_crypt.sql
+++
@@ -1,2 +0,0 @@
--- set password 12345678 crypt
-update tb_user set vl_password = '$2a$10$oEWxlkkKeprHTNajMA1JvuBJ0VB.lQ6jte4A8qLDAlJs8X3ysiW4a';
--- process-backend/bin/src/main/resources/db/migration/V2.2__insert_process_status.sql
+++
@@ -1,3 +0,0 @@
-insert into tb_process_status(vl_description) values ('PENDENTE');
-
-insert into tb_process_status(vl_description) values ('RESPONDIDO');
